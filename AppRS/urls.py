425c45ab0a41886edb1cec14e6c2b06f37674bbd
Rut Zotar <zotar.rut.05tae@gmail.com>
2024-08-28 23:07:19 -0400

    error corregido, add templates-archivos HTML, modificación urls, views

--- AppRS/urls.py
+++ AppRS/urls.py
@@ -2,7 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index),
-    path("saludar/nombre=<str:estudiante>", views.saludar),
-  
+    path('', views.index, name='index'),
+    path('iniciosesion/', views.inicio_sesion, name='iniciosesion'),
+    path('crearcuenta/', views.crear_cuenta, name='crearcuenta'),
+    path('cerrarsesion/', views.cerrar_sesion, name='cerrarsesion'),
+    path('home/', views.posts, name='home')
+    
+    
+    # path("saludar/nombre=<str:estudiante>", views.saludar),
+    # path("",views.prueba),
 ]
--- AppRS/views.py
+++ AppRS/views.py
@@ -6,39 +6,18 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    """Aquí se implementará la lógica para la vista inicial"""
-    posts = [
-        {
-            "id": 1,
-            "title": "Understanding Python Dictionaries",
-            "content": "Dictionaries are powerful data structures in Python...",
-            "author": "John Doe",
-            "date": "2024-08-09",
-            "likes": 0,
-        },
-        {
-            "id": 2,
-            "title": "Why Learn Python?",
-            "content": "Python is a versatile language that can be used for web development...",
-            "author": "Jane Smith",
-            "date": "2024-08-08",
-            "likes": 3,
-        },
-        {
-            "id": 3,
-            "title": "Advanced Python Techniques",
-            "content": "Explore the more advanced features of Python, including generators...",
-            "author": "Alice Johnson",
-            "date": "2024-08-07",
-            "likes": 20,
-        },
-    ]
+    return render(request, 'index.html')
 
-    return render(request, "index.html", {"posts": posts, "program": "crack the code"})
+def crear_cuenta(request):
+    return render(request, 'crearCuenta.html')
+
+def inicio_sesion(request):
+    return render(request, 'inicioSesion.html')
+
+def cerrar_sesion(request):
+    return render(request, 'cerrarSesion.html')
+
+def posts(request):
+    return render(request, 'Publicacion.html')
 
 
-# Utilizando ruta dinámica
-def saludar(request, estudiante):
-    message = f"Bienvenida {estudiante}"
-    data = [1, 2, 3, 4]
-    return HttpResponse(message)
