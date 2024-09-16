17f5ea83a0ef96724410d3a86a2ecd69855c881b
Rut Zotar <zotar.rut.05tae@gmail.com>
2024-09-16 10:47:16 -0400

    subiendo cambios runita

--- AppRS/models.py
+++ AppRS/models.py
@@ -1,3 +1,24 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+class Perfil(models.Model):
+    nombre = models.OneToOneField(User,on_delete=models.CASCADE)
+    biografia = models.TextField(blank=True)
+    foto_perfil = models.ImageField(upload_to='fotos_perfil/', blank=True, null=True)
+    email = models.EmailField()
+    fecha_nacimiento = models.DateField(null=True, blank=True)
+    edad = models.IntegerField()
+    
+    def __str__(self):
+        return self.nombre.User
+    
+    
+class Post(models.Model):
+    autor = models.ForeignKey(User)
+    titulo = models.CharField(max_length=200)
+    descripcion = models.TextField()
+    fecha_post = models.DateTimeField()
+    
+    
+    
